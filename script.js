fb4a3c4100bfbdc800c46da24b6ef931871353f4
DB0807 <dylanbradley87@outlook.com>
2023-06-03 22:14:28 +0100

    create functionality to number buttons and add additional css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let firstNum
-let secondNum
+let displayNum = ''
+let firstNum = parseInt(displayNum)
+let secondNum = 0
 let operator
 
 const operations = {
@@ -12,3 +13,21 @@ const operations = {
 function operate (no1, no2, op) {
     return operations[op](no1,no2)
 }
+
+for (let i = 0; i <= 10; i++) {
+    let numbers = document.querySelector (`.button${i}`)
+    numbers.addEventListener ('click', () => {
+        if (numbers.value === '.' && displayNum.includes('.')) {
+            + ''
+        }
+        else displayNum.length === 9 ? + '' : displayNum += numbers.value.toString();
+        document.querySelector ('.numRowBottom').textContent = displayNum
+})}
+
+const clearDisplay = document.querySelector ('.clear')
+clearDisplay.addEventListener ('click', () => {
+    displayNum = ''
+    document.querySelectorAll('.numRow').forEach((numRow) => {
+        numRow.textContent = '';
+      });
+})
