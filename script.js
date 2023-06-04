99261970e8cadeb1d8f3bf00f9bc35c1aff0a54c
DB0807 <dylanbradley87@outlook.com>
2023-06-04 18:40:27 +0100

    add operate logic for calculator

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//functions for onLoad
+onload = () => clearDisplay();
+
 let displayNum = ''
-let firstNum = parseInt(displayNum)
-let secondNum = 0
-let operator
+let currentNum
+let totalNum = 0
+let operator = ''
 
 const operations = {
     add: (a,b) => a + b,
@@ -11,9 +14,13 @@ const operations = {
 } 
 
 function operate (no1, no2, op) {
-    return operations[op](no1,no2)
+    totalNum = operations[op](no1,no2)
+    displayNum = ''
+    display.textContent = totalNum
+    currentNum = 'stop'
 }
 
+//functionality for buttons to create numbers
 for (let i = 0; i <= 10; i++) {
     let numbers = document.querySelector (`.button${i}`)
     numbers.addEventListener ('click', () => {
@@ -21,13 +28,43 @@ for (let i = 0; i <= 10; i++) {
             + ''
         }
         else displayNum.length === 9 ? + '' : displayNum += numbers.value.toString();
-        document.querySelector ('.numRowBottom').textContent = displayNum
+        currentNum = +displayNum
+        document.querySelector ('.numRow').textContent = currentNum
 })}
 
-const clearDisplay = document.querySelector ('.clear')
-clearDisplay.addEventListener ('click', () => {
-    displayNum = ''
-    document.querySelectorAll('.numRow').forEach((numRow) => {
-        numRow.textContent = '';
-      });
+//function to clear display with button
+function clearDisplay () {
+    const clearDisplay = document.querySelector ('.clear')
+    clearDisplay.addEventListener ('click', () => {
+        displayNum = ''
+        totalNum = 0
+        operator = ''
+        display.textContent = '';
+    });
+}
+
+const display = document.querySelector('.numRow')
+
+const operators = document.querySelectorAll ('.operator').forEach((operatorBtn) => {
+    operatorBtn.addEventListener ('click', () => {
+    operator = operatorBtn.value
+    if (totalNum === 0) {
+        totalNum = currentNum;
+        displayNum = ''} 
+    else if (operator === 'divide' && currentNum === 0) {
+        display.textContent = 'Error: Division by zero';
+    }
+    else if (currentNum === 'stop'){
+        currentNum = 0
+    }
+    else operate (totalNum, currentNum, operator)
+    })
+})
+
+const equals = document.querySelector ('.equals')
+equals.addEventListener ('click', () => {
+    if (operator === 'divide' && currentNum === 0) {
+        display.textContent = 'Error';
+    }
+    else currentNum !== 'stop' ? operate (totalNum, currentNum, operator) : false;
 })
